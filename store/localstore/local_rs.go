8efebd66045df11328488b8bbe4d6f17032a6c08
Ewan Chou <coocood@gmail.com>
2016-03-14 11:05:49 +0800

    xapi/tablecodec: add table codec

--- store/localstore/local_rs.go
+++ store/localstore/local_rs.go
@@ -1,7 +1,20 @@
 package localstore
 
+import (
+	"bytes"
+	"github.com/golang/protobuf/proto"
+	"github.com/juju/errors"
+	"github.com/pingcap/tidb/kv"
+	"github.com/pingcap/tidb/util/hack"
+	"github.com/pingcap/tidb/xapi/tablecodec"
+	"github.com/pingcap/tidb/xapi/tipb"
+	"strconv"
+)
+
 // local region server.
 type localRS struct {
+	startKey []byte
+	endKey   []byte
 }
 
 type regionRequest struct {
@@ -21,5 +34,49 @@ type regionResponse struct {
 }
 
 func (rs *localRS) Handle(req *regionRequest) (*regionResponse, error) {
+	resp := &regionResponse{
+		req: req,
+	}
+	if req.Tp == kv.ReqTypeSelect {
+		sel := new(tipb.SelectRequest)
+		err := proto.Unmarshal(req.data, sel)
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+		rows, err := getRowsFromSelectReq(sel)
+		selResp := new(tipb.SelectResponse)
+		selResp.Error = toPBError(err)
+		selResp.Rows = rows
+		data, err := proto.Marshal(selResp)
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+		resp.data = data
+	}
+	if bytes.Compare(rs.startKey, req.startKey) < 0 || bytes.Compare(rs.endKey, req.endKey) > 0 {
+		resp.newStartKey = rs.startKey
+		resp.newEndKey = rs.endKey
+	}
+	return resp, nil
+}
+
+func getRowsFromSelectReq(sel *tipb.SelectRequest) ([]*tipb.Row, error) {
+	tid := sel.TableInfo.TableId
+	for _, kran := range sel.Ranges {
+		handle, err := parseInt64(kran.Low)
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+		key := tablecodec.EncodeRecordKey(tid, handle)
+
+	}
 	return nil, nil
+}
+
+func parseInt64(b []byte) (int64, error) {
+	return strconv.ParseInt(hack.String(b), 10, 64)
+}
+
+func toPBError(err error) *tipb.Error {
+	return nil
 }
--- xapi/tablecodec/tablecodec.go
+++ xapi/tablecodec/tablecodec.go
@@ -3,17 +3,68 @@ package tablecodec
 import (
 	"github.com/pingcap/tidb/kv"
 	"github.com/pingcap/tidb/util/codec"
+	"github.com/pingcap/tidb/util/types"
+	"github.com/juju/errors"
 )
 
-func EncodeColumnKey(tableId int64, h int64, columnID int64) kv.Key {
+var (
+	TablePrefix = []byte{'t'}
+	recordPrefixSep = []byte("_r")
+	indexPrefixSep  = []byte("_i")
+)
+
+func EncodeRecordKey(tableId int64, h int64, columnID int64) kv.Key {
+	recordPrefix := genTableRecordPrefix(tableId)
 	buf := make([]byte, 0, len(recordPrefix)+16)
 	buf = append(buf, recordPrefix...)
 	buf = codec.EncodeInt(buf, h)
-
 	if columnID != 0 {
 		buf = codec.EncodeInt(buf, columnID)
 	}
 	return buf
 }
 
-func EncodeRecordKey(tableId int64, handle int64)
+func EncodeIndexKey(tableId int64, indexedValues []types.Datum, handle int64, unique bool) (key []byte, distinct bool, err error){
+	if unique {
+		// See: https://dev.mysql.com/doc/refman/5.7/en/create-index.html
+		// A UNIQUE index creates a constraint such that all values in the index must be distinct.
+		// An error occurs if you try to add a new row with a key value that matches an existing row.
+		// For all engines, a UNIQUE index permits multiple NULL values for columns that can contain NULL.
+		distinct = true
+		for _, cv := range indexedValues {
+			if cv.Kind() == types.KindNull {
+				distinct = false
+				break
+			}
+		}
+	}
+	prefix := genTableIndexPrefix(tableId)
+	key = append(key, prefix...)
+	if distinct {
+		key, err = codec.EncodeKey(key, indexedValues...)
+	} else {
+		key, err = codec.EncodeKey(key, append(indexedValues, types.NewDatum(handle))...)
+	}
+	if err != nil {
+		return nil, false, errors.Trace(err)
+	}
+	return key, distinct, nil
+}
+
+// record prefix is "t[tableID]_r"
+func genTableRecordPrefix(tableID int64) kv.Key {
+	buf := make([]byte, 0, len(TablePrefix)+8+len(recordPrefixSep))
+	buf = append(buf, TablePrefix...)
+	buf = codec.EncodeInt(buf, tableID)
+	buf = append(buf, recordPrefixSep...)
+	return buf
+}
+
+// index prefix is "t[tableID]_i"
+func genTableIndexPrefix(tableID int64) kv.Key {
+	buf := make([]byte, 0, len(TablePrefix)+8+len(indexPrefixSep))
+	buf = append(buf, TablePrefix...)
+	buf = codec.EncodeInt(buf, tableID)
+	buf = append(buf, indexPrefixSep...)
+	return buf
+}
