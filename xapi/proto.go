5f2e3fef0691f89cd523bd5198ac118212114200
shenli <shenli3514@gmail.com>
2016-03-15 11:09:27 +0800

    *: WIP

---
+++ xapi/proto.go
@@ -0,0 +1,20 @@
+package xapi
+
+import (
+	"github.com/golang/protobuf/proto"
+	"github.com/juju/errors"
+	"github.com/pingcap/tidb/kv"
+	"github.com/pingcap/tidb/model"
+	"github.com/pingcap/tidb/util/types"
+	"github.com/pingcap/tidb/xapi/tablecodec"
+	"github.com/pingcap/tidb/xapi/tipb"
+)
+
+func ColumnToProto(c *model.ColumnInfo) *tipb.TColumn {
+	/*
+		pb := &tipb.ColumnInfo{
+			ColumnId: proto.Int64(c.ID),
+			Tp:       proto.Int32(c.FieldType.Tp),
+			Collation: proto.
+		}*/
+}
