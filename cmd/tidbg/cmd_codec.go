9505bfb12af30f185077e5ded2e43688ab1dd5e9
zyguan <zhongyangguan@gmail.com>
2024-04-19 08:14:01 +0800

    refine codec util
    Signed-off-by: zyguan <zhongyangguan@gmail.com>

--- cmd/tidbg/cmd_codec.go
+++ cmd/tidbg/cmd_codec.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
 	"strconv"
+	"strings"
+	"text/template"
 
+	"github.com/pingcap/tidb/pkg/types"
+	"github.com/pingcap/tidb/pkg/util/codec"
+	"github.com/pingcap/tidb/pkg/util/collate"
 	"github.com/spf13/cobra"
 )
 
@@ -12,113 +21,111 @@ func newCodecCmd(out Output) *cobra.Command {
 		Use:   "codec",
 		Short: "Codec utilities",
 	}
-	cmd.AddCommand(&cobra.Command{
-		Use:   "quote",
-		Short: "Quote hex keys",
-		Args:  cobra.MinimumNArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			var item struct {
-				In  string `json:"in"`
-				Out string `json:"out,omitempty"`
-				Err string `json:"err,omitempty"`
-			}
-			for _, k := range args {
-				item.In = k
-				raw, err := hex.DecodeString(k)
-				if err != nil {
-					item.Err = err.Error()
-				} else {
-					item.Out = strconv.Quote(string(raw))
-				}
-				if err = out.Dump(item); err != nil {
-					return err
-				}
-			}
-			return nil
-		},
-	})
-	cmd.AddCommand(&cobra.Command{
-		Use:   "unquote",
-		Short: "Unquote string literals",
-		Args:  cobra.MinimumNArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			var item struct {
-				In  string `json:"in"`
-				Out string `json:"out,omitempty"`
-				Err string `json:"err,omitempty"`
-			}
-			for _, k := range args {
-				item.In = k
-				if len(k) > 0 && k[0] != '"' {
-					k = `"` + k + `"`
-				}
-				raw, err := strconv.Unquote(k)
-				if err != nil {
-					item.Err = err.Error()
-				} else {
-					item.Out = hex.EncodeToString([]byte(raw))
-				}
-				if err = out.Dump(item); err != nil {
-					return err
-				}
-			}
-			return nil
-		},
-	})
-	cmd.AddCommand(&cobra.Command{
-		Use:   "encode-mvcc-key",
-		Short: "Encode hex keys to tikv mvcc keys",
-		Args:  cobra.MinimumNArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			var item struct {
-				In  string `json:"in"`
-				Out string `json:"out,omitempty"`
-				Err string `json:"err,omitempty"`
-			}
-			for _, k := range args {
-				item.In = k
-				raw, err := hex.DecodeString(k)
-				if err != nil {
-					item.Err = err.Error()
-				} else {
-					item.Out = hex.EncodeToString(toKvMvccKey(raw))
-				}
-				if err = out.Dump(item); err != nil {
-					return err
-				}
-			}
-			return nil
-		},
-	})
-	cmd.AddCommand(&cobra.Command{
-		Use:   "decode-mvcc-key",
-		Short: "Decode tikv mvcc keys to hex keys",
-		Args:  cobra.MinimumNArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			var item struct {
-				In  string `json:"in"`
-				Out string `json:"out,omitempty"`
-				Err string `json:"err,omitempty"`
-			}
-			for _, k := range args {
-				item.In = k
-				raw, err := hex.DecodeString(k)
-				if err != nil {
-					item.Err = err.Error()
-				} else {
-					key, err := fromKvMvccKey(raw)
-					if err != nil {
-						item.Err = err.Error()
-					} else {
-						item.Out = hex.EncodeToString(key)
-					}
-				}
-				if err = out.Dump(item); err != nil {
-					return err
-				}
-			}
-			return nil
-		},
-	})
+	cmd.AddCommand(newCodecEvalCmd(out))
 	return cmd
+}
+
+func newCodecEvalCmd(out Output) *cobra.Command {
+	var (
+		format       string
+		oldCollation bool
+	)
+	cmd := &cobra.Command{
+		Use:   "eval <template>",
+		Short: "Eval a template that can interact with codec functions",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			buf := new(bytes.Buffer)
+			tmpl, err := template.New("codec").Funcs(template.FuncMap{
+				"quote":   strconv.Quote,
+				"unquote": strconv.Unquote,
+				"trim":    strings.TrimSpace,
+				"hex": func(s string) string {
+					return hex.EncodeToString([]byte(s))
+				},
+				"unhex": func(s string) (string, error) {
+					return tmplBytesError(hex.DecodeString(s))
+				},
+				"key": func(s string) string {
+					return string(codec.EncodeBytes(nil, []byte(s)))
+				},
+				"unkey": func(s string) (string, error) {
+					_, v, err := codec.DecodeBytes([]byte(s), nil)
+					return tmplBytesError(v, err)
+				},
+				"i64": func(i int64) string {
+					return string(codec.EncodeInt(nil, i))
+				},
+				"u64": func(i uint64) string {
+					return string(codec.EncodeUint(nil, i))
+				},
+				"signed": func(i int64) (string, error) {
+					return tmplEncodeKey(types.NewIntDatum(i))
+				},
+				"unsigned": func(i uint64) (string, error) {
+					return tmplEncodeKey(types.NewUintDatum(i))
+				},
+				"float": func(f float64) (string, error) {
+					return tmplEncodeKey(types.NewFloat64Datum(f))
+				},
+				"text": func(s string) (string, error) {
+					return tmplEncodeKey(types.NewStringDatum(s))
+				},
+				"blob": func(s string) (string, error) {
+					return tmplEncodeKey(types.NewBytesDatum([]byte(s)))
+				},
+				"null": func() (string, error) {
+					return tmplEncodeKey(types.Datum{})
+				},
+			}).Parse(args[0])
+			if err != nil {
+				return err
+			}
+
+			if oldCollation {
+				collate.SetNewCollationEnabledForTest(false)
+			}
+
+			input := ""
+			if fi, _ := os.Stdin.Stat(); (fi.Mode() & os.ModeCharDevice) == 0 {
+				in, err := io.ReadAll(os.Stdin)
+				if err != nil {
+					return err
+				}
+				input = strings.TrimSpace(string(in))
+			}
+
+			err = tmpl.Execute(buf, input)
+
+			if err != nil {
+				return err
+			}
+			out.Raw(true)
+			switch format {
+			case "key":
+				out.Dump(hex.EncodeToString(codec.EncodeBytes(nil, buf.Bytes())))
+			case "hex":
+				out.Dump(hex.EncodeToString(buf.Bytes()))
+			case "raw":
+				out.Dump(buf.String())
+			default:
+				return fmt.Errorf("unknown format %q", format)
+			}
+			return nil
+		},
+	}
+	cmd.Flags().StringVarP(&format, "format", "f", "key", "Output format, one of: key, hex, raw.")
+	cmd.Flags().BoolVar(&oldCollation, "old-collation", false, "Use old collation.")
+	return cmd
+}
+
+func tmplBytesError(bs []byte, err error) (string, error) {
+	if err != nil {
+		return "", err
+	}
+	return string(bs), nil
+}
+
+func tmplEncodeKey(d types.Datum) (string, error) {
+	return tmplBytesError(codec.EncodeKey(nil, nil, d))
 }
