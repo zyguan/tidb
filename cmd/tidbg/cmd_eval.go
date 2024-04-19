f8704e3f589e7da8188bf1e1d2f80de835c3cc5f
zyguan <zhongyangguan@gmail.com>
2024-04-19 23:52:11 +0800

    add more builtin funcs
    Signed-off-by: zyguan <zhongyangguan@gmail.com>

--- cmd/tidbg/cmd_eval.go
+++ cmd/tidbg/cmd_eval.go
@@ -15,6 +15,7 @@ import (
 	"github.com/pingcap/tidb/pkg/executor"
 	"github.com/pingcap/tidb/pkg/meta/autoid"
 	"github.com/pingcap/tidb/pkg/parser/model"
+	"github.com/pingcap/tidb/pkg/parser/mysql"
 	"github.com/pingcap/tidb/pkg/types"
 	"github.com/pingcap/tidb/pkg/util/codec"
 	"github.com/pingcap/tidb/pkg/util/collate"
@@ -23,6 +24,124 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/tikv/client-go/v2/oracle"
 )
+
+var builtinFuncs = template.FuncMap{
+	"trim":    strings.TrimSpace,
+	"quote":   quote,
+	"unquote": unquote,
+
+	// functions for parsing domain specific data
+	"decodePlan":       plancodec.DecodePlan,
+	"decodeBinaryPlan": plancodec.DecodeBinaryPlan,
+	"showCreateTable":  tidbShowCreateTable,
+
+	// functions for handling tso
+	"tsoPhysical": oracle.ExtractPhysical,
+	"tsoLogical":  oracle.ExtractLogical,
+	"tsoCompose":  oracle.ComposeTS,
+	"tsoTime": func(tso uint64) string {
+		t := oracle.GetTimeFromTS(tso)
+		return t.Format(time.RFC3339Nano)
+	},
+
+	// functions for common binary codec
+	"hex": func(s string) string {
+		return hex.EncodeToString([]byte(s))
+	},
+	"unhex": func(s string) (string, error) {
+		return tmplBytesError(hex.DecodeString(s))
+	},
+	"key": func(s string) string {
+		return string(codec.EncodeBytes(nil, []byte(s)))
+	},
+	"unkey": func(s string) (string, error) {
+		_, v, err := codec.DecodeBytes([]byte(s), nil)
+		return tmplBytesError(v, err)
+	},
+	"i64": func(i int64) string {
+		return string(codec.EncodeInt(nil, i))
+	},
+	"u64": func(i uint64) string {
+		return string(codec.EncodeUint(nil, i))
+	},
+
+	// functions for encoding datums
+	"signed": func(i int64) (string, error) {
+		return tmplEncodeDatum(types.NewIntDatum(i))
+	},
+	"unsigned": func(i uint64) (string, error) {
+		return tmplEncodeDatum(types.NewUintDatum(i))
+	},
+	"float": func(f float64) (string, error) {
+		return tmplEncodeDatum(types.NewFloat64Datum(f))
+	},
+	"text": func(s string) (string, error) {
+		return tmplEncodeDatum(types.NewStringDatum(s))
+	},
+	"blob": func(s string) (string, error) {
+		return tmplEncodeDatum(types.NewBytesDatum([]byte(s)))
+	},
+	"year": func(y int64) (string, error) {
+		return tmplEncodeTimeDatum(types.ParseTimeFromYear(y))
+	},
+	"datetime": func(s string) (string, error) {
+		return tmplEncodeTimeDatum(types.ParseDatetime(types.DefaultStmtNoWarningContext, s))
+	},
+	"timestamp": func(s string) (string, error) {
+		return tmplEncodeTimeDatum(types.ParseTimestamp(types.DefaultStmtNoWarningContext, s))
+	},
+	"date": func(s string) (string, error) {
+		return tmplEncodeTimeDatum(types.ParseDate(types.DefaultStmtNoWarningContext, s))
+	},
+	"time": func(s string) (string, error) {
+		dur, null, err := types.ParseDuration(types.DefaultStmtNoWarningContext, s, types.GetFsp(s))
+		if err != nil {
+			return "", err
+		}
+		if null {
+			return tmplEncodeDatum(types.Datum{})
+		}
+		return tmplEncodeDatum(types.NewDurationDatum(dur))
+	},
+	"decimal": func(s string) (string, error) {
+		var dec types.MyDecimal
+		if err := dec.FromString([]byte(s)); err != nil {
+			return "", err
+		}
+		return tmplEncodeDatum(types.NewDecimalDatum(&dec))
+	},
+	"enum": func(name, elems string) (string, error) {
+		x, err := types.ParseEnum(strings.Split(elems, ","), name, mysql.DefaultCollationName)
+		if err != nil {
+			return "", err
+		}
+		return tmplEncodeDatum(types.NewMysqlEnumDatum(x))
+	},
+	"set": func(name, elems string) (string, error) {
+		x, err := types.ParseSet(strings.Split(elems, ","), name, mysql.DefaultCollationName)
+		if err != nil {
+			return "", err
+		}
+		return tmplEncodeDatum(types.NewMysqlSetDatum(x, mysql.DefaultCollationName))
+	},
+	"bit": func(s string) (string, error) {
+		x, err := types.ParseBitStr(s)
+		if err != nil {
+			return "", err
+		}
+		return tmplEncodeDatum(types.NewBinaryLiteralDatum(x))
+	},
+	"json": func(s string) (string, error) {
+		x, err := types.ParseBinaryJSONFromString(s)
+		if err != nil {
+			return "", err
+		}
+		return tmplEncodeDatum(types.NewJSONDatum(x))
+	},
+	"null": func() (string, error) {
+		return tmplEncodeDatum(types.Datum{})
+	},
+}
 
 func newEvalCmd(out Output) *cobra.Command {
 	var (
@@ -35,59 +154,7 @@ func newEvalCmd(out Output) *cobra.Command {
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			buf := new(bytes.Buffer)
-			tmpl, err := template.New("codec").Funcs(template.FuncMap{
-				"trim":             strings.TrimSpace,
-				"quote":            quote,
-				"unquote":          unquote,
-				"decodePlan":       plancodec.DecodePlan,
-				"decodeBinaryPlan": plancodec.DecodeBinaryPlan,
-				"showCreateTable":  tidbShowCreateTable,
-				"tsoPhysical":      oracle.ExtractPhysical,
-				"tsoLogical":       oracle.ExtractLogical,
-				"tsoCompose":       oracle.ComposeTS,
-				"tsoTime": func(tso uint64) string {
-					t := oracle.GetTimeFromTS(tso)
-					return t.Format(time.RFC3339Nano)
-				},
-				"hex": func(s string) string {
-					return hex.EncodeToString([]byte(s))
-				},
-				"unhex": func(s string) (string, error) {
-					return tmplBytesError(hex.DecodeString(s))
-				},
-				"key": func(s string) string {
-					return string(codec.EncodeBytes(nil, []byte(s)))
-				},
-				"unkey": func(s string) (string, error) {
-					_, v, err := codec.DecodeBytes([]byte(s), nil)
-					return tmplBytesError(v, err)
-				},
-				"i64": func(i int64) string {
-					return string(codec.EncodeInt(nil, i))
-				},
-				"u64": func(i uint64) string {
-					return string(codec.EncodeUint(nil, i))
-				},
-				"signed": func(i int64) (string, error) {
-					return tmplEncodeKey(types.NewIntDatum(i))
-				},
-				"unsigned": func(i uint64) (string, error) {
-					return tmplEncodeKey(types.NewUintDatum(i))
-				},
-				"float": func(f float64) (string, error) {
-					return tmplEncodeKey(types.NewFloat64Datum(f))
-				},
-				"text": func(s string) (string, error) {
-					return tmplEncodeKey(types.NewStringDatum(s))
-				},
-				"blob": func(s string) (string, error) {
-					return tmplEncodeKey(types.NewBytesDatum([]byte(s)))
-				},
-				"null": func() (string, error) {
-					return tmplEncodeKey(types.Datum{})
-				},
-				// TODO: add more datum encode functions
-			}).Parse(args[0])
+			tmpl, err := template.New("codec").Funcs(builtinFuncs).Parse(args[0])
 			if err != nil {
 				return err
 			}
@@ -148,8 +215,15 @@ func tmplBytesError(bs []byte, err error) (string, error) {
 	return string(bs), nil
 }
 
-func tmplEncodeKey(d types.Datum) (string, error) {
-	return tmplBytesError(codec.EncodeKey(nil, nil, d))
+func tmplEncodeDatum(d types.Datum) (string, error) {
+	return tmplBytesError(codec.EncodeKey(time.UTC, nil, d))
+}
+
+func tmplEncodeTimeDatum(t types.Time, err error) (string, error) {
+	if err != nil {
+		return "", err
+	}
+	return tmplEncodeDatum(types.NewTimeDatum(t))
 }
 
 func tidbShowCreateTable(s string) (string, error) {
@@ -176,9 +250,4 @@ func tidbShowCreateTable(s string) (string, error) {
 		}
 		buf.WriteString(";")
 	}
-}
-
-func tidbTSOTime(tso uint64) string {
-	t := oracle.GetTimeFromTS(tso)
-	return t.Format(time.RFC3339Nano)
 }
